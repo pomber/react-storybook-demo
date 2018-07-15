ab606ad2bfb99fe822dfcc0dc3b0150aa1b2c31e
pomber <pombopombopombo@gmail.com>
2018-07-15 11:52:25 -0300

    Hide side bar

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,11 @@
-import { configure } from '@kadira/storybook';
-import 'todomvc-app-css/index.css'
+import { configure } from "@kadira/storybook";
+import "todomvc-app-css/index.css";
+import { setOptions } from "@kadira/storybook-addon-options";
+setOptions({ showLeftPanel: false });
+console.info("Ctrl + Shift + L to toggle left panel");
 
 function loadStories() {
-  require('../components/stories/');
+  require("../components/stories/");
 }
 
 configure(loadStories, module);
