88821442dfd9cf08b52040417bad1d845cffd56c
Rodrigo Pombo <pombopombopombo@gmail.com>
2018-07-24 12:13:53 -0300

    Edit /actions/index.js

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,7 @@
 import * as types from '../constants/ActionTypes'
 
-export function addTodo(texta) {
-  return { type: types.ADD_TODO, texta }
+export function addTodo(text) {
+  return { type: types.ADD_TODO, text }
 }
 
 export function deleteTodo(id) {
