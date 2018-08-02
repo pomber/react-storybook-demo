ee71f48afa266ca1e17b8eb5a694ae9989cf4c13
Rodrigo Pombo <pombopombopombo@gmail.com>
2018-08-02 17:47:49 -0300

    Edit /test/actions/todos.spec.js

--- test/actions/todos.spec.js
+++ test/actions/todos.spec.js
@@ -13,7 +13,7 @@ describe('todo actions', () => {
   it('deleteTodo should create DELETE_TODO action', () => {
     expect(actions.deleteTodo(1)).toEqual({
       type: types.DELETE_TODO,
-      id: 1
+      id: 2
     })
   })
 
