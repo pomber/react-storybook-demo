a7c709db1b3f4b3752b5f4e9f35f31f9bef8b85c
Rodrigo Pombo <pombopombopombo@gmail.com>
2018-08-04 21:24:40 -0300

    Edit /components/Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import classnames from 'classnames'
 import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters'
 
 const FILTER_TITLES = {
-  [SHOW_ALL]: 'All',
+  [SHOW_ALL]: "All",
   [SHOW_ACTIVE]: 'Active',
   [SHOW_COMPLETED]: 'Completed'
 }
